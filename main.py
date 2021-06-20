4779b6ac00472957e3cc94906bf0f9090d352d52
davidjr01 <davidljr01@gmail.com>
2021-06-19 22:43:36 -0600

    iniciando grafica

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from Ortogonal import matrizOrtogonal
+from tkinter import *
 
-a = matrizOrtogonal()
-a.append(1, 2, "1,2")
-a.append(1, 3, "1,3")
-a.append(2, 3, "2,3")
-a.append(2, 4, "2,4")
-a.Graficar_Nodo()
-    
+ventana=Tk()
+barramenu=Menu(ventana)
+ventana.config(menu=barramenu,width=300 ,height=300)
+amenu=Menu(barramenu,tearoff=0)
+amenu.add_command(label="Abrir partida")
+amenu.add_command(label="Guardar partida")
+amenu.add_command(label="Ayuda")
+jugar=Menu(barramenu)
+barramenu.add_cascade(label="Menu",menu=amenu)
+barramenu.add_cascade(label="Jugar",menu=jugar)
+
+
+
+
+mframe=Frame(ventana,width=800 ,height=400)
+textbox=Entry(mframe)
+textbox.place(x=10,y=30)
+texto=Label(mframe,text="jugador1")
+texto.place(x=10,y=5)
+
+
+def codigoBoton():
+    print(textbox.get())
+boton=Button(mframe,text="ACEPTAR",command=codigoBoton)
+boton.place(x=100,y=100)
+
+mframe.pack()
+
+ventana.mainloop()
+
+
+
  
 
     
