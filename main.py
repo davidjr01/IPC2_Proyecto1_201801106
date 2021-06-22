9def4fe1524f4ba788f09e9670e7fbd6c8a3a88c
davidjr01 <davidljr01@gmail.com>
2021-06-21 18:08:18 -0600

    verificando errores posibles

--- main.py
+++ main.py
@@ -26,22 +26,25 @@ barramenu.add_cascade(label="Menu",menu=amenu)
 barramenu.add_cascade(label="Jugar",menu=jugar)
 
 mframe=Frame(ventana,width=700 ,height=300)
+mframe.config(bg="#ABB2B9")
 
 
 nombre1=Label(mframe,text="Nombre Jugador 1")
 nombre1.place(x=50,y=50)
-nombre1.config()
+nombre1.config(bg="#ABB2B9")
 textbox1=Entry(mframe)
 textbox1.place(x=180,y=50)
 
 
 color1=Label(mframe,text="Color")
 color1.place(x=50,y=90)
+color1.config(bg="#ABB2B9")
 listas1=ttk.Combobox(mframe,values=['Azul','Rojo','Amarillo','Verde'])
 listas1.place(x=180,y=90,width=70,height=20)
 
 color2=Label(mframe,text="Color")
 color2.place(x=350,y=90)
+color2.config(bg="#ABB2B9")
 listas2=ttk.Combobox(mframe,values=['Azul','Rojo','Amarillo','Verde'])
 listas2.place(x=480,y=90,width=70,height=20)
 
@@ -50,19 +53,29 @@ listas2.place(x=480,y=90,width=70,height=20)
 
 nombre2=Label(mframe,text="Nombre Jugador 2")
 nombre2.place(x=350,y=50)
+nombre2.config(bg="#ABB2B9")
 textbox2=Entry(mframe)
 textbox2.place(x=480,y=50)
 
 
 lfila=Label(mframe,text="Fila")
-lfila.place(x=190,y=170)
+lfila.place(x=80,y=170)
+lfila.config(bg="#ABB2B9")
 fila=Entry(mframe)
-fila.place(x=230,y=170,width=70,height=20)
+fila.place(x=140,y=170,width=70,height=20)
 
 lcolumna=Label(mframe,text="Columna")
-lcolumna.place(x=370,y=170)
+lcolumna.place(x=250,y=170)
+lcolumna.config(bg="#ABB2B9")
 columna=Entry(mframe)
-columna.place(x=440,y=170,width=70,height=20)
+columna.place(x=320,y=170,width=70,height=20)
+
+
+ln_piezas=Label(mframe,text="Numero de Piezas")
+ln_piezas.place(x=430,y=170)
+ln_piezas.config(bg="#ABB2B9")
+npiezas=Entry(mframe)
+npiezas.place(x=550,y=170,width=70,height=20)
 
 
 
@@ -89,8 +102,9 @@ def codigoBoton():
         posicion2 = str(anchoventana2) + "x" + str(largoventana2) + "+" + str(x_ventana2) + "+" + str(y_ventana2)
         ventana2.geometry(posicion2)
         ventana2.protocol("WM_DELETE_WINDOW", cerrar_app)
+        ventana2.config(bg="#ABB2B9")
         mframe2=Frame(ventana2,width=600 ,height=600)
-        mframe2.config(bg="#F12222")
+        mframe2.config(bg="#ABB2B9")
         mframe2.place(x=15,y=15)
         fil=fila.get()
         col=columna.get()
@@ -117,24 +131,30 @@ def codigoBoton():
 
         jugador1=Label(ventana2,text="Jugador 1 :"+textbox1.get())
         jugador1.place(x=720,y=260)
+        jugador1.config(bg="#ABB2B9")
         posx1=Label(ventana2,text="Pos X ")
         posx1.place(x=650,y=300)
+        posx1.config(bg="#ABB2B9")
         textox1=Entry(ventana2)
         textox1.place(x=700,y=300,width=70,height=20)
           
         posy1=Label(ventana2,text="Pos Y ")
         posy1.place(x=800,y=300)
+        posy1.config(bg="#ABB2B9")
         textoy1=Entry(ventana2)
         textoy1.place(x=850,y=300,width=70,height=20)
 
         jugador2=Label(ventana2,text="Jugador 2 :"+textbox2.get())
         jugador2.place(x=720,y=400)
+        jugador2.config(bg="#ABB2B9")
         posx2=Label(ventana2,text="Pos X ")
+        posx2.config(bg="#ABB2B9")
         posx2.place(x=650,y=440)
         textox2=Entry(ventana2)
         textox2.place(x=700,y=440,width=70,height=20)
           
         posy2=Label(ventana2,text="Pos Y ")
+        posy2.config(bg="#ABB2B9")
         posy2.place(x=800,y=440)
         textoy2=Entry(ventana2)
         textoy2.place(x=850,y=440,width=70,height=20)
@@ -143,19 +163,25 @@ def codigoBoton():
 
         mframei=Frame(ventana2,width=250 ,height=230)
         mframei.place(x=650,y=15)
+        mframei.config(bg="#ABB2B9")
         ##turno=random.randint(1,6)
         turno=0
         turno2=0
         pos=[]
         pos.append("uno")
         matriz=matrizOrtogonal()
+        cont1=0
+        cont2=0
         
         
         
 
         def iniciarJuego():
             global turno
-            
+            global cont1
+            global cont2
+            cont1=0
+            cont2=0
             
             turno=random.randint(1,6)
             imag = Image.open(str(turno)+".png")
@@ -171,7 +197,12 @@ def codigoBoton():
             
 
         def Empezar():
+            global cont1
             control1=False
+            cont1=cont1+1
+
+            
+
 
             if str(listas1.get())=="Rojo":
                 cb1="red"
@@ -207,8 +238,13 @@ def codigoBoton():
                                 control1=True
                                 break
 
-                if control1==True:
-                    print("no se puede ingresar")
+                if control1==True or cont1==2:
+                    if cont1==2:
+                        print("Perdio Turno")
+                        cont1=0
+                    else:
+                        print("Espacio lleno ")
+                    
                 
                 else: 
                     for i in range(4):
@@ -223,6 +259,7 @@ def codigoBoton():
                             tabla[yc1+i][xc1].config(bg=cb1) 
                             pos.append(str(yc1+i)+str(xc1))
                             matriz.append(xmatriz1,ymatriz1+i,cb1)
+                    cont1=0
 
             elif turno==2:
                 for i in range(4):
@@ -237,8 +274,12 @@ def codigoBoton():
                                 control1=True
                                 break
                 
-                if control1==True:
-                    print("no se puede ingresar")
+                if control1==True or cont1==2:
+                    if cont1==2:
+                        print("Perdio Turno")
+                        cont1=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else: 
                     for i in range(4):
@@ -255,6 +296,9 @@ def codigoBoton():
                             tabla[yc1+i][xc1].config(bg=cb1)
                             pos.append(str(yc1+i)+str(xc1))
                             matriz.append(xmatriz1,ymatriz1+i,cb1)
+
+                    cont1=0
+                
                 
 
             elif turno==3:
@@ -264,14 +308,19 @@ def codigoBoton():
                             control1=True
                             break
                 
-                if control1==True:
-                    print("no se puede ingresar")
+                if control1==True or cont1==2:
+                    if cont1==2:
+                        print("Perdio Turno")
+                        cont1=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
                     for i in range(4):
                         tabla[yc1][xc1+i].config(bg=cb1)  
                         pos.append(str(yc1)+str(xc1+i))
                         matriz.append(xmatriz1+i,ymatriz1,cb1)
+                    cont1=0
 
             
             elif turno==4:
@@ -282,17 +331,22 @@ def codigoBoton():
                             control1=True
                             break
                 
-                if control1==True:
-                    print("no se puede ingresar")
+                if control1==True or cont1==2:
+                    if cont1==2:
+                        print("Perdio Turno")
+                        cont1=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
-                     for i in range(2):
+                    for i in range(2):
                         tabla[yc1][xc1+i].config(bg=cb1) 
                         pos.append(str(yc1)+str(xc1+i))
                         matriz.append(xmatriz1+i,ymatriz1,cb1)
                         tabla[yc1+1][xc1+i].config(bg=cb1) 
                         pos.append(str(yc1+1)+str(xc1+i))
                         matriz.append(xmatriz1+i,ymatriz1+1,cb1)
+                    cont1=0
             
             elif turno==5:
 
@@ -308,8 +362,12 @@ def codigoBoton():
                             control1=True
                             break
                 
-                if control1==True:
-                    print("no se puede ingresar")
+                if control1==True or cont1==2:
+                    if cont1==2:
+                        print("Perdio Turno")
+                        cont1=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
                     for i in range(2):
@@ -321,6 +379,7 @@ def codigoBoton():
                         tabla[yc1+1][xc1-1 +i].config(bg=cb1) 
                         pos.append(str(yc1+1)+str(xc1-1 +i))
                         matriz.append(xmatriz1-1+i,ymatriz1+1,cb1)
+                    cont1=0
             
             elif turno==6:
                 for i in range(5):
@@ -329,33 +388,40 @@ def codigoBoton():
                             control1=True
                             break
                 
-                if control1==True:
-                    print("no se puede ingresar")
+                if control1==True or cont1==2:
+                    if cont1==2:
+                        print("Perdio Turno")
+                        cont1=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
                     for i in range(5):
                         tabla[yc1+i][xc1].config(bg=cb1) 
                         pos.append(str(yc1+i)+str(xc1))
                         matriz.append(xmatriz1,ymatriz1+i,cb1)
-
-
-            textox1.delete(0,END)
-            textoy1.delete(0,END)
-            textox1.config(state="disabled")
-            textoy1.config(state="disabled")
-            textox2.config(state="normal")
-            textoy2.config(state="normal")
-            botonjugar.config(state="disabled")
-            botonjugar2.config(state="normal")
-            global turno2
-            turno2=random.randint(1,6)
-          
-
-            imag = Image.open(str(turno2)+".png")
-            imag1 = ImageTk.PhotoImage(imag)
-            pintarl2 = tk.Label(mframei, image=imag1)
-            pintarl2.imag1=imag1
-            pintarl2.place(x=0, y=0,width=250,height=250)
+                    cont1=0
+            
+            if cont1==True and cont1==1:
+                asdf=0
+                
+            else:
+                textox1.delete(0,END)
+                textoy1.delete(0,END)
+                textox1.config(state="disabled")
+                textoy1.config(state="disabled")
+                textox2.config(state="normal")
+                textoy2.config(state="normal")
+                botonjugar.config(state="disabled")
+                botonjugar2.config(state="normal")
+                global turno2
+                turno2=random.randint(1,6)
+        
+                imag = Image.open(str(turno2)+".png")
+                imag1 = ImageTk.PhotoImage(imag)
+                pintarl2 = tk.Label(mframei, image=imag1)
+                pintarl2.imag1=imag1
+                pintarl2.place(x=0, y=0,width=250,height=250)
 
             
 
@@ -365,6 +431,10 @@ def codigoBoton():
         
 
         def Empezar2():
+            global cont2
+            cont2=cont2+1
+        
+
             control2=False
             if str(listas2.get())=="Rojo":
                 cb2="red"
@@ -400,8 +470,12 @@ def codigoBoton():
                                 control2=True
                                 break
 
-                if control2==True:
-                    print("no se puede ingresar")
+                if control2==True or cont2==2:
+                    if cont2==2:
+                        print("Perdio Turno")
+                        cont2=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else: 
                     for i in range(4):
@@ -416,6 +490,7 @@ def codigoBoton():
                             tabla[yc2+i][xc2].config(bg=cb2) 
                             pos.append(str(yc2+i)+str(xc2))
                             matriz.append(xmatriz2,ymatriz2+i,cb2)
+                    cont2=0
 
             elif turno2==2:
                 for i in range(4):
@@ -430,8 +505,12 @@ def codigoBoton():
                                 control2=True
                                 break
                 
-                if control2==True:
-                    print("no se puede ingresar")
+                if control2==True or cont2==2:
+                    if cont2==2:
+                        print("Perdio Turno")
+                        cont2=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else: 
                     for i in range(4):
@@ -448,6 +527,7 @@ def codigoBoton():
                             tabla[yc2+i][xc2].config(bg=cb2)
                             pos.append(str(yc2+i)+str(xc2))
                             matriz.append(xmatriz2,ymatriz2+i,cb2)
+                    cont2=0
                 
 
             elif turno2==3:
@@ -457,14 +537,19 @@ def codigoBoton():
                             control2=True
                             break
                 
-                if control2==True:
-                    print("no se puede ingresar")
+                if control2==True or cont2==2:
+                    if cont2==2:
+                        print("Perdio Turno")
+                        cont2=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
                     for i in range(4):
                         tabla[yc2][xc2+i].config(bg=cb2)  
                         pos.append(str(yc2)+str(xc2+i))
                         matriz.append(xmatriz2+i,ymatriz2,cb2)
+                    cont2=0
 
             
             elif turno2==4:
@@ -475,17 +560,22 @@ def codigoBoton():
                             control2=True
                             break
                 
-                if control2==True:
-                    print("no se puede ingresar")
+                if control2==True or cont2==2:
+                    if cont2==2:
+                        print("Perdio Turno")
+                        cont2=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
-                     for i in range(2):
+                    for i in range(2):
                         tabla[yc2][xc2+i].config(bg=cb2) 
                         pos.append(str(yc2)+str(xc2+i))
                         matriz.append(xmatriz2+i,ymatriz2,cb2)
                         tabla[yc2+1][xc2+i].config(bg=cb2) 
                         pos.append(str(yc2+1)+str(xc2+i))
                         matriz.append(xmatriz2+i,ymatriz2+1,cb2)
+                    cont2=0
             
             elif turno2==5:
 
@@ -501,8 +591,12 @@ def codigoBoton():
                             control2=True
                             break
                 
-                if control2==True:
-                    print("no se puede ingresar")
+                if control2==True or cont2==2:
+                    if cont2==2:
+                        print("Perdio Turno")
+                        cont2=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
                     for i in range(2):
@@ -514,6 +608,7 @@ def codigoBoton():
                         tabla[yc2+1][xc2-1 +i].config(bg=cb2) 
                         pos.append(str(yc2+1)+str(xc2-1 +i))
                         matriz.append(xmatriz2-1+i,ymatriz2+1,cb2)
+                    cont2=0
             
             elif turno2==6:
                 for i in range(5):
@@ -522,36 +617,44 @@ def codigoBoton():
                             control2=True
                             break
                 
-                if control2==True:
-                    print("no se puede ingresar")
+                if control2==True or cont2==2:
+                    if cont2==2:
+                        print("Perdio Turno")
+                        cont2=0
+                    else:
+                        print("Espacio lleno ")
                 
                 else:
                     for i in range(5):
                         tabla[yc2+i][xc2].config(bg=cb2) 
                         pos.append(str(yc2+i)+str(xc2))
                         matriz.append(xmatriz2,ymatriz2+i,cb2) 
+                    cont2=0
+            if cont2==True and cont1==2:
+                asdf=0
+                
+            else:
+
+                textox2.delete(0,END)
+                textoy2.delete(0,END)
             
+                textox2.config(state="disabled")
+                textoy2.config(state="disabled")
 
-            textox2.delete(0,END)
-            textoy2.delete(0,END)
-            
-            textox2.config(state="disabled")
-            textoy2.config(state="disabled")
+                textox1.config(state="normal")
+                textoy1.config(state="normal")
 
-            textox1.config(state="normal")
-            textoy1.config(state="normal")
+                botonjugar2.config(state="disabled")
+                botonjugar.config(state="normal")
 
-            botonjugar2.config(state="disabled")
-            botonjugar.config(state="normal")
+                global turno
+                turno=random.randint(1,6)
 
-            global turno
-            turno=random.randint(1,6)
-
-            imag = Image.open(str(turno)+".png")
-            imag1 = ImageTk.PhotoImage(imag)
-            pintarl2 = tk.Label(mframei, image=imag1)
-            pintarl2.imag1=imag1
-            pintarl2.place(x=0, y=0,width=250,height=250)
+                imag = Image.open(str(turno)+".png")
+                imag1 = ImageTk.PhotoImage(imag)
+                pintarl2 = tk.Label(mframei, image=imag1)
+                pintarl2.imag1=imag1
+                pintarl2.place(x=0, y=0,width=250,height=250)
         
         def GraficarMatriz():
             matriz.Graficar_Nodo()
